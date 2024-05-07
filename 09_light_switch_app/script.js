53b8f036c120fb5d2637d178a67d45f4e4e8529e
FlausenImKopf <gluecksfeder@mailbox.org>
2024-05-07 09:33:25 +0200

    solution to light switch coding task

--- 09_light_switch_app/script.js
+++ 09_light_switch_app/script.js
@@ -1,11 +1,33 @@
-const btn = document.querySelector("#lumos");
+// Original vanilla-js solution:
 
-btn.addEventListener("click", function () {
-  if (document.body.classList.contains("dark")) {
-    document.body.classList.remove("dark");
-    document.title = "Good Morning";
-  } else {
-    document.body.classList.add("dark");
-    document.title = "Good Night";
-  }
-});
+// const btn = document.querySelector("#lumos");
+
+// btn.addEventListener("click", function () {
+//   if (document.body.classList.contains("dark")) {
+//     document.body.classList.remove("dark");
+//     document.title = "Good Morning";
+//   } else {
+//     document.body.classList.add("dark");
+//     document.title = "Good Night";
+//   }
+// });
+
+// Solution using Vue:
+Vue.createApp({
+  data() {
+    return {
+      text: "Good Morning",
+      onClassName: "dark",
+    };
+  },
+  methods: {
+    active() {
+      this.text === "Good Morning"
+        ? (this.text = "Good Night") &&
+          document.body.classList.add(this.onClassName)
+        : (this.text = "Good Morning") &&
+          document.body.classList.remove(this.onClassName);
+      document.title = this.text;
+    },
+  },
+}).mount("#app");
